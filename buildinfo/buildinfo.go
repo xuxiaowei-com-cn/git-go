74e9431d1997f5f96196811b8c08b7443ac39913
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-11-26 18:35:37 +0800

    :sparkles: 打包时增加版本相关信息

--- buildinfo/buildinfo.go
+++ buildinfo/buildinfo.go
@@ -36,119 +36,123 @@ func init() {
 	goPlatform = GoPlatform()
 }
 
+func Command() []*cli.Command {
+	return []*cli.Command{
+		{
+			Name:    "commit-author",
+			Aliases: []string{"commitAuthor", "ca"},
+			Usage:   "Name <email> 格式的提交作者",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(CommitAuthor())
+				return nil
+			},
+		},
+		{
+			Name:    "commit-branch",
+			Aliases: []string{"commitBranch", "cb"},
+			Usage:   "提交分支名称",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(CommitBranch())
+				return nil
+			},
+		},
+		{
+			Name:    "commit-sha",
+			Aliases: []string{"commitSha", "cs"},
+			Usage:   "项目为其构建的提交修订",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(CommitSha())
+				return nil
+			},
+		},
+		{
+			Name:    "commit-short-sha",
+			Aliases: []string{"commitShortSha", "css"},
+			Usage:   "项目为其构建的提交修订的前八个字符",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(CommitShortSha())
+				return nil
+			},
+		},
+		{
+			Name:    "commit-tag",
+			Aliases: []string{"commitTag", "ct"},
+			Usage:   "提交标签名称",
+			Flags:   []cli.Flag{ExactMatchFlag(), AbbrevFlag(), HeadFlag()},
+			Action: func(cCtx *cli.Context) error {
+				var exactMatch = cCtx.Bool(ExactMatch)
+				var abbrev = cCtx.String(Abbrev)
+				var head = cCtx.Bool(Head)
+				fmt.Println(GitDescribeTags(exactMatch, abbrev, head))
+				return nil
+			},
+		},
+		{
+			Name:    "commit-timestamp",
+			Aliases: []string{"commitTimestamp", "cts"},
+			Usage:   "ISO 8601 格式的提交时间戳，如：2023-10-02T00:29:17+08:00",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(CommitTimestamp())
+				return nil
+			},
+		},
+		{
+			Name:  "now",
+			Usage: "ISO 8601 格式的当前时间戳，如：2023-10-02T02:35:20+08:00",
+			Action: func(cCtx *cli.Context) error {
+				now := time.Now()
+				str := now.Format("2006-01-02T15:04:05+08:00")
+				fmt.Println(str)
+				return nil
+			},
+		},
+		{
+			Name:    "go-version",
+			Aliases: []string{"goVersion", "gv"},
+			Usage:   "go version 命令返回值，如：go version go1.21.1 windows/amd64",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(GoVersion())
+				return nil
+			},
+		},
+		{
+			Name:    "go-short-version",
+			Aliases: []string{"goShortVersion", "gsv"},
+			Usage:   "go version 命令返回值截取版本号，如：go1.21.1",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(GoShortVersion())
+				return nil
+			},
+		},
+		{
+			Name:    "go-platform",
+			Aliases: []string{"goPlatform", "gp"},
+			Usage:   "go version 命令返回值截取平台信息，如：windows/amd64",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(GoPlatform())
+				return nil
+			},
+		},
+		{
+			Name:    "git-tree-state",
+			Aliases: []string{"gitTreeState", "gts"},
+			Usage:   "git 树状态：clean、dirty",
+			Action: func(cCtx *cli.Context) error {
+				fmt.Println(GitTreeState())
+				return nil
+			},
+		},
+	}
+}
+
 func Main() {
 	app := &cli.App{
 		Name:      "git-go",
 		Version:   appVersion,
 		Authors:   []*cli.Author{{Name: Author, Email: Email}},
-		Usage:     "基于 Go 语言的 Git 命令行工具",
+		Usage:     "基于 Go 语言开发的 Git 命令行工具",
 		Copyright: Copyright,
-		Commands: []*cli.Command{
-			{
-				Name:    "commit-author",
-				Aliases: []string{"commitAuthor", "ca"},
-				Usage:   "Name <email> 格式的提交作者",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(CommitAuthor())
-					return nil
-				},
-			},
-			{
-				Name:    "commit-branch",
-				Aliases: []string{"commitBranch", "cb"},
-				Usage:   "提交分支名称",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(CommitBranch())
-					return nil
-				},
-			},
-			{
-				Name:    "commit-sha",
-				Aliases: []string{"commitSha", "cs"},
-				Usage:   "项目为其构建的提交修订",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(CommitSha())
-					return nil
-				},
-			},
-			{
-				Name:    "commit-short-sha",
-				Aliases: []string{"commitShortSha", "css"},
-				Usage:   "项目为其构建的提交修订的前八个字符",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(CommitShortSha())
-					return nil
-				},
-			},
-			{
-				Name:    "commit-tag",
-				Aliases: []string{"commitTag", "ct"},
-				Usage:   "提交标签名称",
-				Flags:   []cli.Flag{ExactMatchFlag(), AbbrevFlag(), HeadFlag()},
-				Action: func(cCtx *cli.Context) error {
-					var exactMatch = cCtx.Bool(ExactMatch)
-					var abbrev = cCtx.String(Abbrev)
-					var head = cCtx.Bool(Head)
-					fmt.Println(GitDescribeTags(exactMatch, abbrev, head))
-					return nil
-				},
-			},
-			{
-				Name:    "commit-timestamp",
-				Aliases: []string{"commitTimestamp", "cts"},
-				Usage:   "ISO 8601 格式的提交时间戳，如：2023-10-02T00:29:17+08:00",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(CommitTimestamp())
-					return nil
-				},
-			},
-			{
-				Name:  "now",
-				Usage: "ISO 8601 格式的当前时间戳，如：2023-10-02T02:35:20+08:00",
-				Action: func(cCtx *cli.Context) error {
-					now := time.Now()
-					str := now.Format("2006-01-02T15:04:05+08:00")
-					fmt.Println(str)
-					return nil
-				},
-			},
-			{
-				Name:    "go-version",
-				Aliases: []string{"goVersion", "gv"},
-				Usage:   "go version 命令返回值，如：go version go1.21.1 windows/amd64",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(GoVersion())
-					return nil
-				},
-			},
-			{
-				Name:    "go-short-version",
-				Aliases: []string{"goShortVersion", "gsv"},
-				Usage:   "go version 命令返回值截取版本号，如：go1.21.1",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(GoShortVersion())
-					return nil
-				},
-			},
-			{
-				Name:    "go-platform",
-				Aliases: []string{"goPlatform", "gp"},
-				Usage:   "go version 命令返回值截取平台信息，如：windows/amd64",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(GoPlatform())
-					return nil
-				},
-			},
-			{
-				Name:    "git-tree-state",
-				Aliases: []string{"gitTreeState", "gts"},
-				Usage:   "git 树状态：clean、dirty",
-				Action: func(cCtx *cli.Context) error {
-					fmt.Println(GitTreeState())
-					return nil
-				},
-			},
-		},
+		Commands:  Command(),
 	}
 
 	if err := app.Run(os.Args); err != nil {
